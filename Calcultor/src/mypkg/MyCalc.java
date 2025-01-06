ec8f0561acd43ef2eb415adfeb963aad5718ac65
kalleshgit <kalleshrudrappamail@gmail.com>
2025-01-06 23:49:41 +0530

    pom file

--- Calcultor/src/mypkg/MyCalc.java
+++ Calcultor/src/mypkg/MyCalc.java
@@ -1,5 +1,44 @@
 package mypkg;
 
 public class MyCalc {
+	
+	int sum (int a, int b)
+	{
+		return a+b;
+	}
+	int diff (int a, int b)
+	{
+		return a-b;
+	}
+	int mul (int a, int b)
+	{
+		return a*b;
+	}
+	int mul2 (int a, int b)
+	{
+		return a*b;
+	}
+	int mul3 (int a, int b)
+	{
+		return a*b;
+	}
+	int mul4 (int a, int b)
+	{
+		return a*b;
+	}int mul5 (int a, int b)
+	{
+		return a*b;
+	}
+
+	public static void main(String[] args) {
+		MyCalc calc= new MyCalc();
+		System.out.println("Sum is : "+calc.sum(20, 10));
+		System.out.println("Diff is : "+calc.diff(20, 10));
+		System.out.println("Diff is : "+calc.mul(20, 10));
+		System.out.println("Diff is : "+calc.mul2(10, 10));
+		System.out.println("Diff is : "+calc.mul3(20, 20));
+		System.out.println("Diff is : "+calc.mul4(30, 40));
+
+	}
 
 }
